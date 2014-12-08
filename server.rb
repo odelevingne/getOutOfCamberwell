6a35130450323331770fe3a23aa247a0b90b92d4
Oliver Delevingne <oliver@delevingne.co.uk>
2014-12-08 22:49:00 +0000

    request for denmark hill info made successfully. displayed at /denmark in in json

--- server.rb
+++ server.rb
@@ -8,3 +8,9 @@ get '/buses' do
   response = HTTParty.get('http://transportapi.com/v3/uk/bus/stop/490006094N/live.json?group=route&api_key=43f13455dd098f1c550d336e10219e83&app_id=b35459a0')
   response.to_json
 end
+
+get '/denmark' do
+  content_type :json
+  response = HTTParty.get('http://transportapi.com/v3/uk/train/station/DMK/live.json?&api_key=43f13455dd098f1c550d336e10219e83&app_id=b35459a0')
+  response.to_json
+end
