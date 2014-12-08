771983784bf882258a901c31c4747636d3569e25
Oliver Delevingne <oliver@delevingne.co.uk>
2014-12-08 22:47:20 +0000

    request for bus info made successfully using httparty. displayed at /buses in json

--- server.rb
+++ server.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'json'
+require 'httparty'
 
-get '/' do
-  
+
+get '/buses' do
+  content_type :json
+  response = HTTParty.get('http://transportapi.com/v3/uk/bus/stop/490006094N/live.json?group=route&api_key=43f13455dd098f1c550d336e10219e83&app_id=b35459a0')
+  response.to_json
 end
