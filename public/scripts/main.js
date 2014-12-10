65d94fb261a37c9f7d8ebb5a4f7c634da80aa76c
Oliver Delevingne <oliver@delevingne.co.uk>
2014-12-10 23:55:54 +0000

    working on displaying bus info in template using mustache

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
 
-  getBusInfo();
+  var busResultsTemplate = $("#bus-results-template");
 
-});
 
-getBusInfo = function() {
+  getBusInfo = function() {
   $.ajax({
       type: 'GET',
       url: '/api/buses',
     success: function(resp) {
       console.log("success!", resp.departures);
+      var renderedBuses = [];
+      var busTemplate = busResultsTemplate.html();
+      $.each(resp.departures, function(i, item){
+        renderedBuses += Mustache.render(busTemplate, item);
+      });
+
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.error("Failure!", jqXHR, textStatus, errorThrown);
     }
   });
 };
+
+
+  getBusInfo();
+
+
+
+});
+
