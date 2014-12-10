253e9eab15b2deb7c90c5c3c21abe44cd7f8f7fb
Oliver Delevingne <oliver@delevingne.co.uk>
2014-12-10 16:03:10 +0000

    changed routes to include API

--- server.rb
+++ server.rb
@@ -3,19 +3,19 @@ require 'json'
 require 'httparty'
 
 
-get '/buses' do
+get '/buses/api' do
   content_type :json
   response = HTTParty.get('http://transportapi.com/v3/uk/bus/stop/490006094N/live.json?group=route&api_key=43f13455dd098f1c550d336e10219e83&app_id=b35459a0')
   response.to_json
 end
 
-get '/denmark' do
+get '/denmark/api' do
   content_type :json
   response = HTTParty.get('http://transportapi.com/v3/uk/train/station/DMK/live.json?&api_key=43f13455dd098f1c550d336e10219e83&app_id=b35459a0')
   response.to_json
 end
 
-get '/dulwich' do
+get '/dulwich/api' do
   content_type :json
   response = HTTParty.get('http://transportapi.com/v3/uk/train/station/EDW/live.json?&api_key=43f13455dd098f1c550d336e10219e83&app_id=b35459a0')
   response.to_json
