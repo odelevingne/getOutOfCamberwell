a36add22e3250e4b74d02382e338c0f8f8a777dd
Oliver Delevingne <oliver@delevingne.co.uk>
2014-12-10 13:59:00 +0000

    changed url on GET request for bus info. fixed cross origin issue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
   getBusInfo = function() {
     $.ajax({
         type: 'GET',
-        url: 'localhost:4567/buses',
+        url: 'http://localhost:4567/buses',
       success: function(resp) {
-        console.info("success!", resp);
+        console.log("success!", resp);
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.error("Failure!", jqXHR, textStatus, errorThrown);
