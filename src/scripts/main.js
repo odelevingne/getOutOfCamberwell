e2a8c019f11e88def99628274eb27005673a3744
Oliver Delevingne <oliver@delevingne.co.uk>
2014-12-10 13:46:14 +0000

    jquery funciton for getting bus info written. issue with cross origin request

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
 
-  $.ajax({
-    type: 'GET',
-    url: 'localhost:4567/denmark',
-  });
-
+  getBusInfo = function() {
+    $.ajax({
+        type: 'GET',
+        url: 'localhost:4567/buses',
+      success: function(resp) {
+        console.info("success!", resp);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error("Failure!", jqXHR, textStatus, errorThrown);
+      }
+    });
+  };
 
 
 });
