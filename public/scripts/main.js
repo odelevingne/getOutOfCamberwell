2d0418e3b59e8d5a30ec20c3b8318a38ac49cd9b
Oliver Delevingne <oliver@delevingne.co.uk>
2014-12-11 23:47:41 +0000

    attempt to render bus times with mustache. small error

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
 
-  var busResultsTemplate = $("#bus-results-template");
-
+  var $busResultsTemplate = $("#bus-results-template");
+  var $busResults = $("#bus-results").find('tbody');
 
   getBusInfo = function() {
-  $.ajax({
-      type: 'GET',
-      url: '/api/buses',
-    success: function(resp) {
-      console.log("success!", resp.departures);
-      var renderedBuses = [];
-      var busTemplate = busResultsTemplate.html();
-      $.each(resp.departures, function(i, item){
-        renderedBuses += Mustache.render(busTemplate, item);
-      });
+    $.ajax({
+        type: 'GET',
+        url: '/api/buses',
+      success: function(resp) {
+        console.log("success!", resp.departures);
 
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-      console.error("Failure!", jqXHR, textStatus, errorThrown);
-    }
-  });
-};
+        var renderedBuses = '';
 
+        var busTemplate = $busResultsTemplate.html();
+
+        $.each(resp, function(i, item){
+          renderedBuses += Mustache.render(busTemplate, item);
+        });
+        $busResults.append(renderedBuses);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error("Failure!", jqXHR, textStatus, errorThrown);
+      }
+    });
+  };
 
   getBusInfo();
-
-
 
 });
 
