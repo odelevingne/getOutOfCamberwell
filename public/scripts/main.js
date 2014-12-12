0879b278deffb37b08a7061f80d90f72230b2a2e
Oliver Delevingne <oliver@delevingne.co.uk>
2014-12-12 16:54:05 +0000

    small naming alterarion. yet to display denmark hill results

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
   var $busResultsTemplate = $("#bus-results-template");
   var $busResults = $("#bus-results").find('tbody');
-  var $trainResultsTemplate = $("#denmark-results-template");
-  var $trainResults = $("#denmark-results").find('denmark-tbody');
+  var $denmarkResultsTemplate = $("#denmark-results-template");
+  var $denmarkResults = $("#denmark-results").find('denmark-tbody');
 
 
   getBusInfo = function() {
@@ -20,7 +20,7 @@ $(document).ready(function(){
         $.each(resp.departures, function(i, item){
           $.each(item, function(i, bus){
             renderedBuses += Mustache.render(busTemplate, bus);
-          })
+          });
         });
         $busResults.append(renderedBuses);
       },
@@ -34,7 +34,7 @@ $(document).ready(function(){
     $.ajax({
       type: 'GET',
       url: '/api/denmark',
-      succes: function(resp) {
+      success: function(resp) {
         console.log("success!", resp.departures.all);
 
         var renderedDenmark = '';
@@ -42,18 +42,16 @@ $(document).ready(function(){
         var denmarkTemplate = $denmarkResultsTemplate.html();
 
         $.each(resp.departures.all, function(i, item){
-          $.each(item, function(i, trains){
-            renderedDenmark += Mustache.render(denmarkTemplate, train);
-          });
+          renderedDenmark += Mustache.render(denmarkTemplate, item);
         });
-        $denmakResults.append(renderedDenmark);
+        $denmarkResults.append(renderedDenmark);
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.error("Failure!", jqXHR, textStatus, errorThrown);
       }
     });
   };
-  
+
   getDenmarkHillInfo();
   getBusInfo();
 
